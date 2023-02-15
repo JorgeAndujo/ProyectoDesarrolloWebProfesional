7f0dd784b5e1f04502ed5487de02252f780d33ef
Jorge Andujo <jandujo@interaxis.com.mx>
2023-02-14 20:01:12 -0700

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RoutesDir from './RoutesDir';
+
+function App() {
+  return (
+    <>
+      <RoutesDir />
+    </>
+  );
+}
+
+export default App;
---
+++ src/RoutesDir.jsx
@@ -0,0 +1,9 @@
+import {useLocation} from "react-router-dom"
+
+const listaDePaginas = ["/login", "/registrar-usuario", "/recuperar-contrasenia"]
+
+const RoutesDir = () => {
+    const location = useLocation()
+}
+
+export default RoutesDir;
